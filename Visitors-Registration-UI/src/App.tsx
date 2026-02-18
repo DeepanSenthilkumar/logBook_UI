f99d70fc5a2f9d4e7b5aa390bc600b495df557ef
Deepan <deepan.s@icodexsolutions.com>
2026-02-18 11:00:24 +0530

    removed features page

--- Visitors-Registration-UI/src/App.tsx
+++ Visitors-Registration-UI/src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home.tsx'
-// import Features from './pages/Features.tsx'
 import Navbar from './components/navbar/Navbar.tsx'
 import Visitor from './pages/visitor/Visitor.tsx'
 import Login from './pages/login/Login.tsx'
@@ -27,7 +26,6 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/visitor" element={<Visitor />} />
-              {/* <Route path="/features" element={<Features />} /> */}
               <Route path="/login" element={<Login />} />
               <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
             </Routes>
